ad78b25c478cfcb9f6c030fc049ff84c8b129791
nata1228 <91463907+nata1228@users.noreply.github.com>
2022-08-18 18:21:15 +0900

    Initial commit

---
+++ resources/js/Pages/Admin.vue
@@ -0,0 +1,16 @@
+<template>
+  <layout title="ブランド一覧">
+    <h1>Hoge</h1>
+  </layout>
+</template>
+
+<script>
+  import AdminLayout from '@/Layouts/AdminLayout.vue'
+
+  export default {
+    layout: AdminLayout,
+    props: {
+      //
+    },
+  }
+</script>
