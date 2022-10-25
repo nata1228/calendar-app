7a35091a22791241297c22f21968826890a981fe
Takumi <>
2022-10-25 21:35:33 +0900

    schedules tableを追加しました。

---
+++ database/migrations/2022_10_25_123021_create_schedules_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->time('start_time');
+            $table->string('schedule_name');
+            $table->integer('button_color');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+};
